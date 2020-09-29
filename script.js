30f5185783efebe53e4491215a21534b110a0939
Nikolai Justesen <nikifymusic@gmail.com>
2020-09-29 18:14:50 +0200

    Swapped in local background images and tweaked animations

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-window.addEventListener('load', function () {
-    document.getElementById("bg_img").style.opacity = 0.3;
-    document.getElementById("gradient").style.opacity = 1;
-    document.getElementById("icon_container").style.bottom = "6%";
-    document.getElementById("icon_container").style.opacity = 1;
-  })
+window.addEventListener('load', function () { 
+    init();
+});
+
+  function init(){
+    document.getElementById("bg_img").src = "resources/images/" + getRandomInt(12) + ".jpg";
+    setTimeout(function(){
+      document.getElementById("bg_img").style.opacity = 0.3;
+      document.getElementById("gradient").style.opacity = 1;
+      document.getElementById("icon_container").style.top = "calc(50% - (200px/2))";
+      document.getElementById("icon_container").style.opacity = 1;
+      document.getElementById("logo").style.opacity = 1;
+    }, 200);
+  }
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
